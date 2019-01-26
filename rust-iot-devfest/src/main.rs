1cbc258d091b3b9b690f3e18738f0895c2eacf91
Daniel Bank <daniel.p.bank@gmail.com>
2019-01-26 13:31:41 -0700

    Sinwave again

--- rust-iot-devfest/src/main.rs
+++ rust-iot-devfest/src/main.rs
@@ -1,8 +1,17 @@
-mod text;
-
-use crate::text::Display;
-use light_morse::*;
+use rodio::{
+    default_output_device, play_raw,
+    source::{SineWave, Source},
+};
+use std::time::Duration;
 
 fn main() {
-    "Morse".to_string().to_morse(MorseType::Gerke).display();
+    let device = default_output_device().unwrap();
+    let source1 = SineWave::new(300);
+    let source2 = SineWave::new(600);
+    let short = source1.take_duration(Duration::from_millis(1000));
+    play_raw(&device, short.convert_samples());
+    let short = source2.take_duration(Duration::from_millis(1000));
+    play_raw(&device, short.convert_samples());
+
+    loop {}
 }
